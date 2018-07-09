45d254a1bb360402778fd7a4d6d639135366daa9
Anouarsoufyani <anouar.soufyani@gmail.com>
2018-07-09 16:34:10 -0400

    Remplacement du fichier pig_latin par la version finale

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,59 +1,38 @@
-# #write your code here
-# def translate(a)
-# voyelles = []
-# consonnes = []
-# compteurV = 0
-# motsRecomp = ""
-#
-#   a.scan(/\w+/).map do |c|
-#     c.scan(/./).map.with_index do |char, i |
-#
-#       if i == 0 && char =~ /[aeiou]/
-#         compteurV += 1
-#         voyelles[c] += char
-#       else
-#         consonnes[c] += char
-#       end
-#
-#       if i == 1 && char =~ /[aeiou]/ && compteurV < 1
-#         compteurV += 1
-#         voyelles[c] += char
-#       else
-#         consonnes[c] += char
-#       end
-#
-#       if i == 2 && char =~ /[aeiou]/ && compteurV < 1
-#         compteurV += 1
-#         voyelles[c] += char
-#       else
-#         consonnes[c] += char
-#       end
-#       if i != 0 && i != 1 && i != 2
-#         voyelles[c] += char
-#       end
-#   motsRecomp += voyelles[c] + consonnes[c] + " "
-#     end
-#
-#   end
-#   motsRecomp.chop.insert(-1 , "ay")
-# end
-# puts translate("dfdfsoudf sdifsdaf")
-
 #write your code here
-def translate(a)
-voyelles = []
-consonnes = []
-compteurV = 0
-motsRecomp = ""
-
-  a.scan(/\w+/).map do |c|
-    c.scan(/./).map.with_index do |char, i |
-
-      if i == 0 && char =~ /[aeiou]/
-        c + "ay"
-      end
-    end
-
+#définition de la méthode translate
+def translate(string)
+  #transforme la string en array
+array = string.split(" ")
+ #on applique la fonction à chaque mots
+pigged_array = array.map! {|x| pigify(x)}
+#on transformel'array en string
+result = pigged_array.join(" ")
+#on retourne la valeur
+return result
+end
+#définition de la méthode pigify
+def pigify(word)
+  #définition des voyelles
+  vowels = ["a", "e", "i", "o", "u"]
+  #si la première lettre est une voyelle, on ajoute "ay" au mot
+  if vowels.include? word[0].downcase
+    word + "ay"
+  # 2 cas pour "qu"
+  #si un mot commence "qu" on séléctionne de la 3eme à la dernière lettre en enlevant le "qu" et ajoute "quay" à la fin
+elsif word[0..1] == "qu" #quiet
+    #2 est la 3eme lettre et -1 est la dernière lettre
+    word[2..-1] + "quay"
+  #si un mot commence par "qu" avec word[3..-1] on sélectionne le "are" le word[0..2] on prend le squ + ay
+  elsif word[1..2] == "qu"
+    word[3..-1] + word[0..2] + "ay"
+  #Pour les mots de 3 consonnes
+  elsif !(vowels.include? word[0]) && !(vowels.include? word[1]) && !(vowels.include? word[2])
+    word[3..-1] + word[0..2] + "ay"
+    #Pour les mots de 2 consonnes
+  elsif !(vowels.include? word[0]) && !(vowels.include? word[1])
+    word[2..-1] + word[0..1] + "ay"
+    #Pour les mots de 1 consonnes
+  else
+    word[1..-1] + word[0] + "ay"
   end
 end
-puts translate("afdfsoudf adifsdaf")
